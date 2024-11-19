2a05ae3ed9d82712e243a405faa8cb2f3950228e
DryTurtle391 <binayakway@gmail.com>
2024-11-19 09:58:44 +0530

    added collide method in Pipe class

--- flappy_bird.py
+++ flappy_bird.py
@@ -145,7 +145,29 @@ class Pipe:
         win.blit(self.PIPE_TOP, (self.x, self.top))
         win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
 
-    # def collide(self, bird, win):
+    def collide(self, bird, win):
+        """Checks collision between bird and pipes"""
+        # Here we use a concept of mask to check collision
+        # since images are drawn as rectangles, it is important we use masks
+        # on the images to determine pixel perfect collisions
+
+        # Creating masks for birds and pipes
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+        # Creating offsets which tells the distance between bird and pipes
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        # variables checking if collision has occured
+        t_point = bird_mask.overlap(top_mask, top_offset)
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+
+        if t_point or b_point:
+            return True
+
+        return False
 
 
 def draw_window(win, bird):
